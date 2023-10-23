5c1664d46fb2143bbd1a5d10e2e348fbce3553bf
pai_jk <dlwnsrb05@publicai.co.kr>
2023-10-23 17:32:10 +0900

    fetch

---
+++ question/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'question'
+
+urlpatterns = [
+    path('hello_world/', views.hello_world, name='hello_world'),
+    path('create/', views.question_create, name='create'),
+    path('index/', views.index, name='index'),
+]
